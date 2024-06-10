283020a39c1b2d3d9a57fd34bcf5eb3f29e34b77
jsakthivel <sak.jeeva123@gmail.com>
2024-06-11 00:13:39 +0530

    bug fixed

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -16,86 +16,78 @@ const addExercise = asyncHandler(async (req, res) => {
     };
     const isValidForm = checkValidFormField(exerciseObj);
     if (Object.entries(isValidForm).length === 0) {
-        const { username } = await user.findById(req.params._id);
-        let { userId, description, duration, date } = await exercise.create(
-            exerciseObj
-        );
-        res.json({ _id: userId, username, description, duration, date });
+        try {
+            const result = await user.findById(req.params._id);
+            if (!result) {
+                throw new Error();
+            }
+            let { userId, description, duration, date } = await exercise.create(
+                exerciseObj
+            );
+            res.json({
+                _id: userId,
+                username: result.username,
+                description,
+                duration,
+                date,
+            });
+        } catch (error) {
+            throw new Error(`User with ID ${req.params._id} not found `);
+        }
     } else {
         res.status(STATUS_CODE.CLIENT_ERROR_RESPONSE_CODE).json(isValidForm);
     }
 });
 
-// Getting the Exercises
+// getting exercises with filters
 const getExercises = asyncHandler(async (req, res) => {
     const { _id, username } = await getUserDetail(req.params._id);
-    let resultArr = await getUserExercises(_id);
-    if (req?.query) {
-        resultArr = getLimitedExercises(resultArr.exercises, req.query);
+    let query = { userId: _id };
+
+    let exercises = [];
+    let count = 0;
+
+    if (req.query) {
+        const isValidDateformat = isValidDate(req.query);
+        const { from, to, limit } = req.query;
+
+        if (!isValidDateformat) {
+            if (from && !to) {
+                query.date = { $gte: new Date(from) }; // $gte for greater than or equal to
+            } else if (!from && to) {
+                const endDate = new Date(to);
+                endDate.setHours(23, 59, 59, 999);
+                query.date = { $lte: endDate }; // $lte for less than or equal to
+            } else if (from && to) {
+                const endDate = new Date(to);
+                endDate.setHours(23, 59, 59, 999);
+                query.date = { $gte: new Date(from), $lte: endDate };
+            }
+        }
+
+        const result = await exercise
+            .find(query)
+            .sort({ date: 1 })
+            .limit(limit || 10);
+
+        exercises = result.map((obj) => {
+            const { _id, description, duration, date } = obj;
+            return { _id, description, duration, date };
+        });
+        count = await exercise.countDocuments(query); // Get total count without limit
     }
-    const { exercises, count } = resultArr;
-    const result = {
+
+    const response = {
         _id,
         username,
         count,
         logs: exercises,
     };
 
-    res.json(result);
+    res.json(response);
 });
-
-//Getting Exercises of the Respective user
-const getUserExercises = asyncHandler(async (id) => {
-    const result = await exercise.find({ userId: id }).sort({ date: 1 });
-    const filteredExercise = result.map((obj) => {
-        const { _id, description, duration, date } = obj;
-        return { _id, description, duration, date };
-    });
-    const count = filteredExercise.length;
-    return { exercises: filteredExercise, count };
-});
-
-//Getting Exercises of The Respective user with some limited exercises
-const getLimitedExercises = (inputExercise, query) => {
-    const isValidDateformat = isValidDate(query);
-    if (!isValidDateformat) {
-        const { from = "", to = "", limit = "" } = query;
-        let startDate = from ? new Date(from) : null;
-        let endDate = to ? new Date(to) : null;
-        if (endDate) {
-            // Set end date to the end of the day
-            endDate.setHours(23, 59, 59, 999);
-        }
-
-        let results = [];
-
-        if (startDate && !endDate) {
-            results = inputExercise?.filter(
-                (exercise) => exercise.date >= startDate
-            );
-        } else if (!startDate && endDate) {
-            results = inputExercise?.filter(
-                (exercise) => exercise.date <= endDate
-            );
-        } else if (startDate && endDate) {
-            results = inputExercise?.filter(
-                (exercise) =>
-                    exercise.date >= startDate && exercise.date <= endDate
-            );
-        } else {
-            results = inputExercise;
-        }
-
-        const limitedExercise = limit ? results.slice(0, limit) : results;
-
-        const count = results.length;
-        return { exercises: limitedExercise, count };
-    }
-};
 
 module.exports = {
     addExercise,
     getExercises,
-    getUserExercises,
-    getLimitedExercises,
 };
